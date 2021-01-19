41e02d38b080d7be6fcdd9c12ac47135023294ab
JaroslawFrankiewicz <frantz12@wp.pl>
2021-01-19 18:37:21 +0100

    19.01.2021#1

--- src/main/resources/META-INF/sql/data.sql
+++
@@ -1,6 +0,0 @@
-use charity_donation;
-INSERT INTO role (id, name) VALUES (NULL,'ROLE_USER');
-INSERT INTO role (id, name) VALUES (NULL, 'ROLE_ADMIN');
-
-INSERT INTO users (email, firstName, lastName, username, password) VALUES ('frantz12@wp.pl', 'Jarosław', 'Frankiewicz', 'frantz', '$2y$12$pCt916J9z.ntKQne.7ArYeoVkKBV.Z4Qfoz63QQN/mDHA4JzQ5lX6');
-INSERT INTO user_role (ROLE_ID, USER_ID) VALUES (2,1);
