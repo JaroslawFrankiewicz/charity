c61822b3d99d092aa01c06a885711034bbd31b81
JaroslawFrankiewicz <frantz12@wp.pl>
2021-01-20 19:20:42 +0100

    19.01.2021#Poprawna data!

--- src/main/java/pl/coderslab/charity/CharityApplication.java
+++ src/main/java/pl/coderslab/charity/CharityApplication.java
@@ -3,11 +3,18 @@ package pl.coderslab.charity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CharityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CharityApplication.class, args);
     }
-
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot SetTimeZone
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
