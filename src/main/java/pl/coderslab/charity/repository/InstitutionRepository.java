f62d2c1c4d4c5254c749e6a62a9949e0456a8d2e
JaroslawFrankiewicz <frantz12@wp.pl>
2021-02-16 20:19:30 +0100

    16.02.2021#Poprawki

--- src/main/java/pl/coderslab/charity/repository/InstitutionRepository.java
+++ src/main/java/pl/coderslab/charity/repository/InstitutionRepository.java
@@ -2,7 +2,6 @@ package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import pl.coderslab.charity.entity.Institution;
 
 import java.util.List;
